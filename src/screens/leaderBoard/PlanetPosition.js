1949e6f7e38910e1756dc5e7655ba13ecc7c2941
prandeveloper <pranaydevelopersveltosest@gmail.com>
2022-12-05 15:10:56 +0530

    meteors

--- src/screens/leaderBoard/PlanetPosition.js
+++ src/screens/leaderBoard/PlanetPosition.js
@@ -1,67 +1,28 @@
 import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {FlatGrid} from 'react-native-super-grid';
+import axios from 'axios';
 
 const PlanetPosition = () => {
-  const [items, setItems] = React.useState([
-    {
-      text1: 'Sun',
-      text2: '0-500',
-      price: '$10',
-      imageUrl: require('../../assets/images/planet/sun.png'),
-    },
+  const [items, setItems] = useState();
 
-    {
-      text1: 'Mercury',
-      text2: '500-1000',
-      price: '$15',
-      imageUrl: require('../../assets/images/planet/mercury.png'),
-    },
-    {
-      text1: 'Venus',
-      text2: '1000-2000',
-      price: '$18',
-      imageUrl: require('../../assets/images/planet/venus.png'),
-    },
-    {
-      text1: 'Mars',
-      text2: '2000-5000',
-      price: '$20',
-      imageUrl: require('../../assets/images/planet/mars.png'),
-    },
-    {
-      text1: 'Jupiter',
-      text2: '5000-7500',
-      price: '$25',
-      imageUrl: require('../../assets/images/planet/jupiter.png'),
-    },
-    {
-      text1: 'Saturn',
-      text2: '7500-10000',
-      price: '$40',
-      imageUrl: require('../../assets/images/planet/saturn.png'),
-    },
-    {
-      text1: 'Uranus',
-      text2: '10000-20000',
-      price: '$50',
-      imageUrl: require('../../assets/images/planet/uranus.png'),
-    },
-    {
-      text1: 'Neptune',
-      text2: '20000-50000',
-      price: '$100',
-      imageUrl: require('../../assets/images/planet/neptune.png'),
-    },
-    {
-      text1: 'Earth',
-      text2: '50000+',
-      price: '$1000',
-      imageUrl: require('../../assets/images/planet/earth.png'),
-    },
-  ]);
+  const getPlanetPosition = () => {
+    axios
+      .get(`http://3.7.173.138:9000/user/user_planet_position`)
+      .then(response => {
+        // console.log(response.data.data);
+        setItems(response.data.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getPlanetPosition();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text
@@ -79,13 +40,16 @@ const PlanetPosition = () => {
         renderItem={({item}) => (
           <View style={styles.innerView}>
             <View style={styles.iconView}>
-              <Image source={item.imageUrl} style={styles.planetImage} />
+              <Image
+                source={{uri: `${item?.img}`}}
+                style={styles.planetImage}
+              />
             </View>
 
             <View>
-              <Text style={styles.upperText}>{item.text1}</Text>
-              <Text style={styles.lowerText}>{item.text2}</Text>
-              <Text style={styles.dollar}>{item.price}</Text>
+              <Text style={styles.upperText}>{item?.planet_name}</Text>
+              <Text style={styles.lowerText}>{item?.point_range}</Text>
+              <Text style={styles.dollar}>{item?.doller_rupees}</Text>
             </View>
           </View>
         )}
@@ -138,3 +102,61 @@ const styles = StyleSheet.create({
     margin: 2,
   },
 });
+
+// [
+//   {
+//     text1: 'Sun',
+//     text2: '0-500',
+//     price: '$10',
+//     imageUrl: require('../../assets/images/planet/sun.png'),
+//   },
+
+//   {
+//     text1: 'Mercury',
+//     text2: '500-1000',
+//     price: '$15',
+//     imageUrl: require('../../assets/images/planet/mercury.png'),
+//   },
+//   {
+//     text1: 'Venus',
+//     text2: '1000-2000',
+//     price: '$18',
+//     imageUrl: require('../../assets/images/planet/venus.png'),
+//   },
+//   {
+//     text1: 'Mars',
+//     text2: '2000-5000',
+//     price: '$20',
+//     imageUrl: require('../../assets/images/planet/mars.png'),
+//   },
+//   {
+//     text1: 'Jupiter',
+//     text2: '5000-7500',
+//     price: '$25',
+//     imageUrl: require('../../assets/images/planet/jupiter.png'),
+//   },
+//   {
+//     text1: 'Saturn',
+//     text2: '7500-10000',
+//     price: '$40',
+//     imageUrl: require('../../assets/images/planet/saturn.png'),
+//   },
+//   {
+//     text1: 'Uranus',
+//     text2: '10000-20000',
+//     price: '$50',
+//     imageUrl: require('../../assets/images/planet/uranus.png'),
+//   },
+//   {
+//     text1: 'Neptune',
+//     text2: '20000-50000',
+//     price: '$100',
+//     imageUrl: require('../../assets/images/planet/neptune.png'),
+//   },
+//   {
+//     text1: 'Earth',
+//     text2: '50000+',
+//     price: '$1000',
+//     imageUrl: require('../../assets/images/planet/earth.png'),
+//   },
+// ];
